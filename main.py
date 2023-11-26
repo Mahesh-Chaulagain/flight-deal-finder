5d734dc43865216998a561f870e72538ab0bdd14
Mahesh-Chaulagain <maheshchaulagain8@gmail.com>
2023-11-26 08:11:18 +0545

    send message if cheap flights found

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from data_manager import DataManager
+from datetime import datetime, timedelta
+from flight_search import FlightSearch
+from notification_manager import NotificationManager
+
 data_manager = DataManager()
+flight_search = FlightSearch()
+notification_manager = NotificationManager()
+
+ORIGIN_CITY_IATA = "LON"
+
 sheet_data = data_manager.get_destination_data()
 
 # if IATA column is empty pass each city name in sheet_data to flight search to get correspondinng IATA code
@@ -7,11 +16,26 @@ sheet_data = data_manager.get_destination_data()
 # International Air Transport Association (IATA) code helps to identify airports and metropolitan areas
 
 if sheet_data[0]["iataCode"] == "":     # check for "iatacode" is empty
-    from flight_search import FlightSearch
-    flight_search = FlightSearch()
     for row in sheet_data:
         row["iataCode"] = flight_search.get_destination_code(row["city"])
-    print(sheet_data)
 
     data_manager.destination_data = sheet_data
     data_manager.update_destination_codes()
+
+today = datetime.now()
+six_month_from_today = datetime.now() + timedelta(days=(6*30))
+
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=today,
+        to_time=six_month_from_today
+    )
+    if flight is not None and flight.price < destination["lowestPrice"]:
+        notification_manager.send_sms(
+            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} "
+                    f"to {flight.destination_city}-{flight.destination_airport},"
+                    f"from {flight.out_date} to {flight.return_date}."
+        )
+
--- notification_manager.py
+++ notification_manager.py
@@ -1,3 +1,23 @@
+from twilio.rest import Client
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TWILIO_SID = os.getenv("TWILIO_SID")
+TWILIO_AUTH_TOKEN = os.getenv("AUTH_TOKEN")
+
+
 class NotificationManager:
     # This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=os.getenv("FROM_NUMBER"),
+            to=os.getenv("TO_NUMBER")
+        )
+        # print if successfully sent
+        print(message.sid)
+
